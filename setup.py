7e80d647026384b7e6db14705de169d500d7e187
Corey Oordt <coreyoordt@gmail.com>
2012-07-07 10:15:07 -0500

    Updated the setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
 import os
 from setuptools import setup, find_packages
 
+
 def read_file(filename):
+    """Read a file into a string"""
     path = os.path.abspath(os.path.dirname(__file__))
     filepath = os.path.join(path, filename)
     try:
@@ -9,17 +11,29 @@ def read_file(filename):
     except IOError:
         return ''
 
+
+def get_readme():
+    """Return the README file contents. Supports text,rst, and markdown"""
+    for name in ('README', 'README.rst', 'README.md'):
+        if os.path.exists(name):
+            return read_file(name)
+    return ''
+
+# Use the docstring of the __init__ file to be the description
+DESC = " ".join(__import__('inlaws').__doc__.splitlines()).strip()
+
+
 setup(
-    name = "django-inlaws",
-    version = __import__('inlaws').get_version().replace(' ', '-'),
-    url = 'http://github.com/washingtontimes/django-inlaws',
-    author = 'Corey Oordt, Jose Soares, Justin Quick',
-    author_email = 'webdev@washingtontimes.com',
-    description = 'Shows related objects in the Django admin.',
-    long_description = read_file('README.rst'),
-    packages = find_packages(),
-    include_package_data = True,
+    name="django-inlaws",
+    version=__import__('inlaws').get_version().replace(' ', '-'),
+    url='http://github.com/callowayproject/django-inlaws',
+    author='Corey Oordt, Jose Soares, Justin Quick',
+    author_email='coreyoordt@gmail.com',
+    description=DESC,
+    long_description=read_file('README.rst'),
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=read_file('requirements.txt'),
-    classifiers = [
+    classifiers=[
     ],
 )
