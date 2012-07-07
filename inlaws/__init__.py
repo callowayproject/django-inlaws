d9305a75e388806c3087d13e1c5196f17409cd5b
Corey Oordt <coreyoordt@gmail.com>
2012-07-07 10:34:50 -0500

    Version bump to 0.2b1

--- inlaws/__init__.py
+++ inlaws/__init__.py
@@ -1,18 +1,24 @@
+"""
+When you are editing an object, Django Inlaws shows a sidebar containing objects
+related to it in the database. It consists of a replacement template for the
+Admin's change form and some template tags.
+"""
 __version_info__ = {
     'major': 0,
-    'minor': 1,
-    'micro': 1,
+    'minor': 2,
+    'micro': 0,
     'releaselevel': 'beta',
     'serial': 1
 }
 
-def get_version():
-    vers = ["%(major)i.%(minor)i" % __version_info__, ]
 
+def get_version(short=False):
+    assert __version_info__['releaselevel'] in ('alpha', 'beta', 'final')
+    vers = ["%(major)i.%(minor)i" % __version_info__, ]
     if __version_info__['micro']:
         vers.append(".%(micro)i" % __version_info__)
-    if __version_info__['releaselevel'] != 'final':
-        vers.append('%(releaselevel)s%(serial)i' % __version_info__)
+    if __version_info__['releaselevel'] != 'final' and not short:
+        vers.append('%s%i' % (__version_info__['releaselevel'][0], __version_info__['serial']))
     return ''.join(vers)
 
 __version__ = get_version()
